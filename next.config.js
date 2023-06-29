4c13f0e6499c2492538fef8ba6cee517abce8654
Jordy <jordy.lee@centr.com>
2023-06-29 15:17:17 +1000

    fix: second attempt at build fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
+const trueEnv = ['true', '1', 'yes'];
+
+const NEXTJS_IGNORE_ESLINT = trueEnv.includes(
+    process.env?.NEXTJS_IGNORE_ESLINT ?? 'false',
+);
+const NEXTJS_IGNORE_TYPECHECK = trueEnv.includes(
+    process.env?.NEXTJS_IGNORE_TYPECHECK ?? 'false',
+);
+
 module.exports = {
+  typescript: {
+    ignoreBuildErrors: NEXTJS_IGNORE_TYPECHECK,
+  },
+  eslint: {
+    ignoreDuringBuilds: NEXTJS_IGNORE_ESLINT,
+    dirs: ['src'],
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     domains: ['res.cloudinary.com'],
